01512673907bb2fd5e5360649b5170a1157f4813
veronica-debug <121700906+veronica-debug@users.noreply.github.com>
2024-02-20 16:30:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,10 @@ def main():
 def image_gpt():
     return(render_template("image_gpt.html"))
 
+@app.route("/end",methods=["GET","POST"])
+def end():
+    first_time = 1
+    return(render_template("end.html"))
+
 if __name__ == "__main__":
       app.run()
