22d65e76b123239a542dc2dd5ab2c87155821052
veronica-debug <121700906+veronica-debug@users.noreply.github.com>
2024-02-21 10:27:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask,request,render_template
+import replicate
+import os
+import time
 
 app=Flask(__name__)
+os.environ["REPLICATE_API_TOKEN"] = "r8_ct15bbEKjKiJNYKjmc9L0hSfFl5VJ931k5iAf"
 
 r = ""
 first_time = 1
@@ -20,6 +24,17 @@ def main():
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
     return(render_template("image_gpt.html"))
+
+@app.route("/image_result",methods=["GET","POST"])
+def image_result():
+    q = request.form.get("q")
+    r = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+                  input={
+                      "prompt": q
+                  })
+    
+    time.sleep(10)
+    return(render_template("image_result.html",r=r[0]))
 
 @app.route("/end",methods=["GET","POST"])
 def end():
