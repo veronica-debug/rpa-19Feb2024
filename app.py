cfa1afc9001112592a784d7d4391665ba960aad2
veronica-debug <121700906+veronica-debug@users.noreply.github.com>
2024-02-22 11:32:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask,request,render_template
 import replicate
 import os
 import time
-from openai import OpenAI
 
-openai_api_key=os.environ["OPENAI_API_TOKEN"]="sk-qx4zKKzHnYRc7k8LuXyKHXdQi0gf5BcisgXBVZcXc9t4K8E9"
-os.environ["REPLICATE_API_TOKEN"]="r8_ct15bbEKjKiJNYKjmc9L0hSfFl5VJ931k5iAf"
-model = OpenAI(api_key=openai_api_key, base_url="https://api.chatanywhere.tech/v1")
+os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
+
 app = Flask(__name__)
 
 r = ""
@@ -24,6 +22,15 @@ def main():
         first_time=0
     return(render_template("main.html",r=r))
 
+@app.route("/text_gpt",methods=["GET","POST"])
+def text_gpt():
+    return(render_template("text_gpt.html"))
+
+@app.route("/text_result",methods=["GET","POST"])
+def text_result():
+    q = request.form.get("q")
+    return(render_template("text_result.html",r="API not ready"))
+
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
     return(render_template("image_gpt.html"))
@@ -40,30 +47,11 @@ def image_result():
     time.sleep(10)
     return(render_template("image_result.html",r=r[0]))
 
-@app.route("/text_gpt",methods=["GET","POST"])
-def text_gpt():
-    return(render_template("text_gpt.html"))
-
-@app.route("/text_result",methods=["GET","POST"])
-def text_result():
-    q=input("question:")
-    r = model.chat.completions.create(
-        model="gpt-3.5-turbo", 
-        messages=[
-            {
-                "role": "user",
-                 "content": q
-            }
-        ],
-    )
-    time.sleep(5)
-    return(render_template("text_result.html",r=r.choices[0].message.content))
-
 @app.route("/end",methods=["GET","POST"])
 def end():
-    global first_time
+    global first_time,r
     first_time = 1
-    return(render_template("end.html"))
+    return(render_template("end.html",r=r))
 
 if __name__ == "__main__":
     app.run()
